8b95073fa19f063deb5f39b98d1f835c2644d6b1
elektroid <bozo@nobodix.org>
2015-12-10 13:55:45 +0100

    a couple of changes

--- resizer.go
+++ resizer.go
@@ -34,6 +34,20 @@ func exitWithResult(isSuccess bool, strCode string, details error) error {
 	return nil //we are dead anyway
 }
 
+func getImageDimension(imagePath string) (int, int, error) {
+    file, err := os.Open(imagePath)
+    if err != nil {
+        return 0,0,fmt.Errorf("%v\n", err)
+    }
+
+    image, _, err := image.DecodeConfig(file)
+    if err != nil {
+        return 0,0,fmt.Errorf("%s: %v\n", imagePath, err)
+    }
+    return image.Width, image.Height, nil
+}
+
+
 // file copy done with io.Copy
 func copy(input string, output string) error {
 	// open files r and w
@@ -71,23 +85,28 @@ func outputFormat(inputFormat string) string {
 }
 
 // open a file and decode it into a *image.Image
-func loadImage(inputFile string) (*image.Image, string, error) {
+func loadImage(inputFile string) (*image.Image, string, int64, error) {
 
 	file, err := os.Open(inputFile)
 	if err != nil {
-		return nil, "", err
+		return nil, "", 0,err
 	}
 	defer file.Close()
 
+	fi, err := file.Stat()
+	if err != nil {
+    	return nil, "", 0, err
+	}
+
 	img, ftype, err := image.Decode(file)
 	if err != nil {
-		return nil, ftype, err
+		return nil, ftype, 0,err
 	}
-	return &img, ftype, err
+	return &img, ftype,fi.Size(), err
 }
 
 // encode/write a *image.Image to outputFile into outputFormat
-func encode(outputFormat string, outputFile string, m image.Image) error {
+func encode(outputFormat string, outputFile string, m image.Image, jpegQuality int) error {
 
 	out, err := os.Create(outputFile)
 	if err != nil {
@@ -98,9 +117,10 @@ func encode(outputFormat string, outputFile string, m image.Image) error {
 	if outputFormat == "gif" {
 		return gif.Encode(out, m, nil)
 	} else if outputFormat == "png" {
-		return png.Encode(out, m)
+		encoder:=png.Encoder{CompressionLevel:png.BestCompression }
+		return encoder.Encode(out, m)
 	} else if outputFormat == "jpeg" {
-		jpgOptions := jpeg.Options{Quality: 85}
+		jpgOptions := jpeg.Options{Quality: jpegQuality}
 		return jpeg.Encode(out, m, &jpgOptions)
 	}
 
@@ -113,6 +133,7 @@ func main() {
 	// if width and height are set to zero, we blindly copy file
 	width := flag.Uint("width", 0, "width, pass 0 if no constraint")
 	height := flag.Uint("height", 0, "height, pass 0 if no constraint")
+	quality := flag.Int("quality", 85, "export jpg quality, 85 by default (other formats not affected)")
 	inputFile := flag.String("input", "", "input path")
 	outputFile := flag.String("output", "", "output path (directory must exist)")
 	flag.Parse()
@@ -120,7 +141,7 @@ func main() {
 	
 	// we do not support bmp as golang.org/x/image/bmp seems buggy
 	// all other formats are enabled by importing /image/ libraries.
-	img, originalFormat, err := loadImage(*inputFile)
+	img, originalFormat, fileSize, err := loadImage(*inputFile)
 	if err != nil {
 		exitWithResult(false, "openImage", err)
 		return
@@ -135,19 +156,32 @@ func main() {
 		exitWithResult(true, "file copied", nil)
 	}
 
+	w,h, err :=getImageDimension(*inputFile)
+	if err != nil {
+		exitWithResult(false, "getImageDimension", err)
+	}
 	// if no value is given for a dimension, we keep current 
 	if *height == 0 {
-		*height = uint((*img).Bounds().Max.Y)
+		*height = uint(h)
 	}
 	if *width == 0 {
-		*width = uint((*img).Bounds().Max.X)
+		*width = uint(w)
+	}
+
+	// if original dimension fits max, just copy
+	if *height>=uint(h) && *width>=uint(w) && fileSize< 150000{
+		err := copy(*inputFile, *outputFile)
+		if err != nil {
+			exitWithResult(false, "flatCopy", err)
+		}
+		exitWithResult(true, "file copied", nil)
 	}
 
 	// dimensions are treated like max, proportions are kept
 	m := resize.Thumbnail(*width, *height, *img, resize.Lanczos3)
 
 	// write new image in chosen format
-	err = encode(outputFormat(originalFormat), *outputFile, m)
+	err = encode(outputFormat(originalFormat), *outputFile, m, *quality)
 	if err != nil {
 		exitWithResult(false, "writeImage", err)
 	} else {
